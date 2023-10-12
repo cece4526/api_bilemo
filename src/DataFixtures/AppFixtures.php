fabc2a0e243fd0383bd6b313b6952e816e3444e5
cece4526 <cece4526@hotmail.fr>
2023-10-12 21:35:38 +0200

    create fixtures for testing API

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,15 +2,52 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Customer;
+use App\Entity\Product;
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
+    public function __construct(UserPasswordHasherInterface $userPasswordHasher)
+    {
+        $this->userPasswordHasher = $userPasswordHasher;
+    }
+
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        for ($i=0; $i < 5; $i++) { 
+            $customer = new Customer();
+            $customer->setName('entreprise'.$i);
+            $customer->setRoles(["ROLE_ADMIN"]);
+            $customer->setEmail('customer'.$i.'@customer.fr');
+            $customer->setPassword($this->userPasswordHasher->hashPassword($customer, "password"));
+            $manager->persist($customer);
+            $listCustomer[] = $customer;
+        }
+        for ($i=0; $i < 10; $i++) { 
+            $user = new User();
+            $user->setFirstname('user'.$i);
+            $user->setLastname('lastname'.$i);
+            $user->setRoles(["ROLE_ADMIN"]);
+            $user->setEmail('user'.$i.'@user.fr');
+            $user->setPassword($this->userPasswordHasher->hashPassword($user, "password"));
+            $user->setCustomer($listCustomer[array_rand($listCustomer)]);
+            $manager->persist($user);
+
+        }
+        for ($i=0; $i < 10; $i++) { 
+            $product = new Product();
+            $product->setBrand('marque'. $i);
+            $product->setModel('model'. $i);
+            $product->setPrice('prix'. $i);
+            $product->setColor('couleur'. $i);
+            $product->setDescription('description du smartphone n¤'. $i);
+            $manager->persist($product);
+        }
+        
 
         $manager->flush();
     }
