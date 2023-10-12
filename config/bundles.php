4ab6bf9f9919c600f01477c75416a63f9d44da62
cece4526 <cece4526@hotmail.fr>
2023-10-12 18:37:28 +0200

    install jwt,nelmio,twig,symfony\security and validator, hateos bundle

--- config/bundles.php
+++ config/bundles.php
@@ -8,4 +8,7 @@ return [
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
     Bazinga\Bundle\HateoasBundle\BazingaHateoasBundle::class => ['all' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
+    Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
 ];
